1553e4ff9030db64430cf1b798e0a0ddc8cd331a
Hasindu Koshitha <koshithahasindu@gmail.com>
2025-08-10 22:35:43 +0530

    move app into app.dart (separate main.dart and app.dart )

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'features/home/home_screen.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bottom Nav – Two Pages',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,4 @@
 import 'package:flutter/material.dart';
-import 'page1.dart';
-import 'page2.dart';
+import 'app.dart';
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Two Pages Example',
-      initialRoute: '/page1',
-
-      routes: {'/page1': (context) => Page1(), '/page2': (context) => Page2()},
-    );
-  }
-}
+void main() => runApp(const App());
